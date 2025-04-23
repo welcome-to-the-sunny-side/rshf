c672be875dc1779d49243a98c92227e3da8b933f
shinji ikari <evapilotno17@gmail.com>
2025-04-23 21:40:14 +0530

    added reset_db to utils

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,16 @@
 import hashlib, os, base64
+from app.database import Base, engine
+from app import models
+
+def reset_db():
+    print("dropping all tables...")
+    Base.metadata.drop_all(bind=engine)
+    print("all tables dropped.")
+
+    print("creating tables from models...")
+    Base.metadata.create_all(bind=engine)
+    print("schema rebuilt.")
+
 
 def hash_password(password: str) -> str:
     salt = os.urandom(16)
