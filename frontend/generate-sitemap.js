72ecde0a81c90e77646ff23fb23aa7e2b7fe6536
welcome-to-the-sunny-side <nonadhocproblems@gmail.com>
2025-06-03 18:04:25 +0530

    changes

--- frontend/generate-sitemap.js
+++ frontend/generate-sitemap.js
@@ -2,7 +2,7 @@
 const { SitemapStream, streamToPromise } = require('sitemap');
 const { createWriteStream } = require('fs');
 
-const sitemap = new SitemapStream({ hostname: 'https://your-domain.com' }); // TODO: Replace with your domain
+const sitemap = new SitemapStream({ hostname: 'https://rshf.net' }); // TODO: Replace with your domain
 
 // List your static routes here
 const routes = [
